08cceee21877a0b654f5cc96cffdb693a85d5b3f
Natallia <n.gorbunova@softteco.com>
2016-07-07 01:39:45 +0300

    CommentList

---
+++ src/CommentList.js
@@ -0,0 +1,39 @@
+import React, { Component }  from 'react';
+import Comment from './Comment';
+
+class CommentList extends Component{
+    state = {
+        isOpen:false
+    }
+
+
+    render() {
+        const {comments} = this.props;
+        const { isOpen } = this.state;
+        let listItems, title;
+        
+        if(comments) {
+            listItems =  isOpen ? comments.map((comment) => <li key={comment.id}><Comment comment={comment}/></li>) : null;
+            title = isOpen ? 'Collapse Comment list' : 'Expand comment list';
+        }else{
+            title = null;
+            listItems = null;
+        }
+
+        return(
+        <div>
+            <h3 onClick = {this.toggleOpen}>{title}</h3>
+            <ul>
+                {listItems}
+            </ul>
+        </div>)
+    };
+
+    toggleOpen = (ev) =>{
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+}
+
+export default CommentList;
