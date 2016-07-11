8648493b296265e9b219f38e189320674657d9c4
natallia <gorbunovano@gmail.com>
2016-07-11 12:48:21 +0300

    Decorator: done + allow article to be closed

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import { articles as articles } from './fixtures'
+import ArticleList from './ArticleList'
 import React from 'react'
 import { render } from 'react-dom'
 import Root from './containers/Root'
